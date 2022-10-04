668aa29f8a5b5ee9365215a7b0db58d02bf7bbbf
ddururiiiiiii <o244227@daum.net>
2022-10-04 11:12:08 +0900

    first commit

--- src/main/java/chap01/Max3_221004.java
+++ src/main/java/chap01/Max3_221004.java
@@ -1,5 +1,20 @@
 package chap01;
 
-public class Max3_221004 {
+import java.util.Scanner;
 
+public class Max3_221004 {
+    // 세개의 정수값을 입력하고 최댓값을 구하여 출력.
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("세 정수의 최댓값을 구합니다.");
+        System.out.println("a의 값 : "); int a = scan.nextInt();
+        System.out.println("b의 값 : "); int b = scan.nextInt();
+        System.out.println("c의 값 : "); int c = scan.nextInt();
+
+
+
+    }
 }
