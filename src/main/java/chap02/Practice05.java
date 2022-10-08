823882f10455bc76378da7d496592b8d7a050082
ddururiiiiiii <o244227@daum.net>
2022-10-08 19:35:48 +0900

    2. 배열 - 배열요소 복사, 역순으로 만들기

--- src/main/java/chap02/Practice05.java
+++ src/main/java/chap02/Practice05.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 class Practice05 {
 
-    //--- 배열 b의 모든 요소를 배열 a에 역순으로 copy  ----//
+    //---- 배열 b의 모든 요소를 배열 a에 역순으로 copy  ----//
     static void rcopy(int[] a, int[] b) {
         int num = a.length <= b.length ? a.length : b.length;
         for (int i = 0; i < num; i++)
